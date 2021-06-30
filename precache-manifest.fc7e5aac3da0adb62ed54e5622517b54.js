e2ae5dbad2b0db51595fd740c7925efe4df4d29b
ujalakhurram <ujala.khurram@venturedive.com>
2021-06-30 11:33:56 +0500

    Updates

---
+++ precache-manifest.fc7e5aac3da0adb62ed54e5622517b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8773c132259dbd9fa6e5fd871aa8db",
+    "url": "/grocery-bud/index.html"
+  },
+  {
+    "revision": "1c55cbd7d5f1ffa12d2f",
+    "url": "/grocery-bud/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "8dc858ad2f5199b0af5a",
+    "url": "/grocery-bud/static/js/2.2486421f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud/static/js/2.2486421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c55cbd7d5f1ffa12d2f",
+    "url": "/grocery-bud/static/js/main.03f319ed.chunk.js"
+  },
+  {
+    "revision": "24104236d27962cef5d9",
+    "url": "/grocery-bud/static/js/runtime-main.74f22f2c.js"
+  }
+]);
